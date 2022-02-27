e004607b44ff729c7fa8757e6f3f0e76fd22b342
Cody <cody.rod93@gmail.com>
2022-02-27 15:21:54 -0800

    refactor game logic

--- public/index.js
+++ public/index.js
@@ -16,17 +16,13 @@ class Dot {
 		const [gameBoard] = document.getElementsByTagName('body');
 
 		const dotElem = document.createElement('span');
-		dotElem.style.display = 'inline-block';
-		dotElem.style.position = 'absolute';
+
 		dotElem.style.height = `${size}px`;
 		dotElem.style.width = `${size}px`;
-		dotElem.style.margin = '0';
-		dotElem.style.border = '2px solid black';
-		dotElem.style.borderRadius = '50%';
-		dotElem.style.top = 0;
+
 		// dotElem.setAttribute('visibility', 'hidden'); // hide dot until shown on game board
 		dotElem.setAttribute('id', id.toString());
-		//TODO: add dot class
+		dotElem.classList.add('dot');
 		gameBoard.appendChild(dotElem);
 
 		this.size = size;
@@ -52,35 +48,92 @@ class Dot {
 class Game {
 	constructor(numberOfDots) {
 		const [gameBoard] = document.getElementsByTagName('body');
-		//TODO: double check if this is the correct way of setting values
+
 		this.gameBoard = {
 			height: gameBoard.offsetHeight,
 			width: gameBoard.offsetWidth,
 		};
+		this.gameRef = null;
 		this.score = 0;
 		this.isPlaying = false;
-		this.speed = 0;
+		this.speed = 10;
 		this.NUMBER_OF_DOTS = numberOfDots;
 		this.dots = [];
+
+		// add game DOM controllers
+		const startButton = document.getElementById('start-btn');
+
+		startButton.addEventListener('click', (event) => {
+			console.log(event);
+			if (!this.isPlaying) {
+				startButton.textContent = 'Pause';
+
+				this.play();
+			} else {
+				startButton.textContent = 'Start';
+				this.stop();
+			}
+			this.isPlaying = !this.isPlaying;
+		});
+
+		// speed dial
+		const speedDial = slider.querySelector('.slider-dial');
+		const speedDisplay = document.getElementById('speed');
+		speedDisplay.textContent = this.speed;
+
+		speedDial.onmousedown = (event) => {
+			event.preventDefault(); // prevent selection start (browser action)
+
+			let shiftX = event.clientX - speedDial.getBoundingClientRect().left;
+
+			const onMouseMove = (event) => {
+				let newLeft =
+					event.clientX - shiftX - slider.getBoundingClientRect().left;
+
+				// the pointer is out of slider => lock the thumb within the bounaries
+				if (newLeft < 0) {
+					newLeft = 0;
+				}
+				let rightEdge = slider.offsetWidth - speedDial.offsetWidth;
+				if (newLeft > rightEdge) {
+					newLeft = rightEdge;
+				}
+				// TODO: get this to work correctly
+
+				this.speed = parseInt(newLeft / 10);
+				console.log(this.speed);
+				speedDisplay.textContent = this.speed;
+				speedDial.style.left = newLeft + 'px';
+			};
+
+			const onMouseUp = () => {
+				document.removeEventListener('mouseup', onMouseUp);
+				document.removeEventListener('mousemove', onMouseMove);
+			};
+
+			document.addEventListener('mousemove', onMouseMove);
+			document.addEventListener('mouseup', onMouseUp);
+		};
+
+		speedDial.ondragstart = () => {
+			return false;
+		};
 	}
 
 	play() {
 		this.createDots();
 
-		// let frameID;
-		// const getDiv = document.getElementsByClassName('frame');
-		// const smoothAnimation = () => {
-		// 	getDiv[0].insertAdjacentHTML('afterend', "<div class='frame'></div>");
-		// 	frameID = requestAnimationFrame(smoothAnimation);
-		// };
-
-		// const onStart = () => {
-		// 	frameID = requestAnimationFrame(smoothAnimation);
-		// };
-
-		// const onStop = () => {
-		// 	cancelAnimationFrame(frameID);
-		// };
+		this.dots.forEach((dot) => {
+			let dotElem = document.getElementById(dot.id);
+			let startPos = 0;
+			const maxPos = this.gameBoard.height;
+			const moveStone = () => {
+				startPos += this.speed;
+				dotElem.style.top = (startPos % maxPos) + 'px';
+				requestAnimationFrame(moveStone);
+			};
+			requestAnimationFrame(moveStone);
+		});
 	}
 
 	createDots() {
@@ -105,54 +158,20 @@ class Game {
 
 	positionDots() {
 		this.dots.forEach((dot) => {
-			//TODO: check if these values have to be unique
 			const randPosition = randomIntFromInterval(
 				0,
 				this.gameBoard.width - dot.size
 			);
-			console.log(randPosition);
+
 			dot.setPosition(randPosition);
 		});
 	}
 
 	moveDots() {}
+
+	stop() {
+		console.log('stop game');
+	}
 }
 
-const game = new Game(10);
-game.play();
-//TODO: refactor this
-let thumb = slider.querySelector('.slider-dial');
-
-thumb.onmousedown = function (event) {
-	event.preventDefault(); // prevent selection start (browser action)
-
-	let shiftX = event.clientX - thumb.getBoundingClientRect().left;
-	// shiftY not needed, the thumb moves only horizontally
-
-	document.addEventListener('mousemove', onMouseMove);
-	document.addEventListener('mouseup', onMouseUp);
-
-	function onMouseMove(event) {
-		let newLeft = event.clientX - shiftX - slider.getBoundingClientRect().left;
-
-		// the pointer is out of slider => lock the thumb within the bounaries
-		if (newLeft < 0) {
-			newLeft = 0;
-		}
-		let rightEdge = slider.offsetWidth - thumb.offsetWidth;
-		if (newLeft > rightEdge) {
-			newLeft = rightEdge;
-		}
-
-		thumb.style.left = newLeft + 'px';
-	}
-
-	function onMouseUp() {
-		document.removeEventListener('mouseup', onMouseUp);
-		document.removeEventListener('mousemove', onMouseMove);
-	}
-};
-
-thumb.ondragstart = function () {
-	return false;
-};
+new Game(10);
